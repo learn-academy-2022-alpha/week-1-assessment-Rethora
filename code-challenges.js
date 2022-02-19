3b83179d280e6d9822027ef985552a2a52b5feb3
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-02-18 19:27:13 -0800

    finished the assessment

--- code-challenges.js
+++ code-challenges.js
@@ -14,6 +14,34 @@ const fruit2 = "banana"
 const fruit3 = "cherry"
 const fruit4 = "kiwi"
 
+//  Pseudo Code
+// create a function named longerString
+// longerString will take two arguments (stringOne and stringTwo) both of type string
+// we will have a three conditions to check
+// 1) we will check if the length of stringOne is equal to length of stringTwo
+// if true, we will return that they are the same length
+// 2) we will check if the length of stringOne is greater than the length of stringTwo
+// if true, we will return stringOne
+// 3) we can use a catch all that will execute on all other occasions
+// the only other condition that it would be is that the length of stringTwo is greater than the length of stringOne. So, we will return stringTwo
+
+// Code
+const longerString = (stringOne, stringTwo) => {
+    if (stringOne.length === stringTwo.length) {
+        return "The strings are the same length."
+    }
+    else if (stringOne.length > stringTwo.length) {
+        return stringOne
+    }
+    else {
+        return stringTwo
+    }
+}
+
+console.log(longerString(fruit1, fruit3))
+// console.log(longerString("the same length", "the same length"))
+// console.log(longerString("the longer string", "taco"))
+
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
@@ -21,22 +49,86 @@ const temp1 = 42
 const temp2 = 350
 const temp3 = 212
 
+// Pseudo Code
+// Create a function named howHot
+// howHot will take a single argument (temp) of the type Integer
+// we can check three conditions here
+// 1) if the temp is equal to 212
+// if so we can return "<temp> is at boiling point"
+// 2) if the temp is less than 212
+// if so we can return "<temp> is below boiling point"
+// 3) now we can use a catch all because the only other case would be that temp is greater than 212
+// return "<temp> is above boiling point"
+
+// Code
+const howHot = (temp) => {
+    if (temp === 212) {
+        return `${temp} is at boiling point`
+    }
+    else if (temp < 212) {
+        return `${temp} is below boiling point`
+    }
+    else {
+        return `${temp} is above boiling point`
+    }
+}
+
+console.log(howHot(temp1))
+console.log(howHot(temp2))
+console.log(howHot(temp3))
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+// Pseudo Code
+// create a variable named concattedArray
+// assign concattedArray to the two arrays merged together
+// get the length of concattedArray
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
+const concattedArray = myNumbers1.concat(myNumbers2)
+console.log(concattedArray.length)
+
+// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 ahplA"
 
 const currentCohort = "Alpha 2022"
 
+// Pseudo Code
+// create a variable named stringArr
+// assign stringArr to an array of strings with the content of currentCohort split apart
+// reverse the values of stringArr
+// create a variable named reversedString
+// assign reversedString with the contents of stringArr put back together into a string
+
+// Code
+const stringArr = currentCohort.split("")
+stringArr.reverse()
+const reversedString = stringArr.join("")
+console.log(reversedString)
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
 const myArray = [13, 34, 5, 10, 27, 42]
 
+// Pseudo Code
+// create a loop that will iterate over myArray
+// for each element of myArray we will check for two conditions
+// 1) we will check if the number is even
+// if true, we will log "even"
+// 2) we can use a catch all because whatever number is not even must be odd
+// log "odd"
+
+// Code
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    if (element % 2 === 0) {
+        console.log("even")
+    }
+    else {
+        console.log("odd")
+    }
+}
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
@@ -47,3 +139,28 @@ const number2 = 100
 // Set two:
 const number3 = 27
 const number4 = 24
+
+// Pseudo Code
+// create a function named subtractLarger
+// subtractLarger will take two arguments (numOne, numTwo) of type Number
+// we will have two more variables store our smaller and larger number (larger, smaller)
+// we can use a conditional compare the two numbers
+// if numOne is greater than or equal to numTwo: assign numOne to larger and numTwo to smaller (this will handle if both numbers are the same)
+// else numTwo is greater and numOne is smaller
+// we can subtract larger from smaller
+
+// Code
+const subtractLarger = (numOne, numTwo) => {
+    let larger, smaller
+    if (numOne >= numTwo) {
+        [larger, smaller] = [numOne, numTwo]
+    }
+    else {
+        [larger, smaller] = [numTwo, numOne]
+    }
+    return larger - smaller
+}
+
+console.log(subtractLarger(number1, number2))
+console.log(subtractLarger(number3, number4))
+// console.log(subtractLarger(100, 100))
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,5 @@
 # ASSESSMENT 1: Tech Interview Practice Questions
+
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory as if you were in a job interview. If you don't know the answer a good tip is to answer a simpler version of the question.
@@ -7,62 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-  Your answer:
+  Your answer: A conditional statement checks for a boolean (value of true or false). If this condition evaluates to true, then it will execute the code block following it. However if it evaluates to false, it will skip the code block following it and check the next condition (if there is one).
 
-  Researched answer:
-
-
+  Researched answer: A conditional statement is used for checking a specific condition and executing code based on that condition. The types of conditional statements are if, else, else if, and switch-case (which I definitely knew and should have included in my answer). I could have probably also included that the syntax of these are the keyword followed by parentheses (with the condition inside) and followed curly braces (with the code inside of those).
 
 1. What is git? What is the difference between git and Github?
 
-  Your answer:
+  Your answer: Git is a versioning control software. It allows developers to make changes to a code base on their local machine while a working version is intact. Delvelopers can update the code on their local and make changes to it without directly effecting the working code. With that being said, Github can be thought of like the "Cloud". This is were code can be inspected and approved to be merged into the main branch (working code). It's also a way to communicate what needs work or to be changed before it is merged with the main code.
 
-  Researched answer:
-
-
+  Researched answer: Git is a free open source version control software. It is useful for staging areas and multiple workflows. Git allows multiple collaborators to work on a project simultaneously from different local machines.
 
 2. Which JavaScript operators will return a Boolean value?
 
-  Your answer:
+  Your answer: Logical operators will return a boolean value. Some Logical Operators include strict equality, loose equality, AND(&&) and OR(||).
 
-  Researched answer:
-
-
+  Researched answer: So, obviously I need some work with operator terms and in general. There are equality, relational and logical operators. Equality operators include strict (===) and loose (==). Relational operators compares relationship between two values(> and <). Logical operators include and (&&), or (||) and not(!). These will all return a boolean that can be useful for checking certain conditions.
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: An index is the position of an element in a collection. Computers start counting at zero, so the index of the first element in an array would be zero. A value would be the actual piece of data in the collection.
 
-  Researched answer:
-
-
+  Researched answer: Indices are read-only properties that indicate the position of the element within an array.
 
 4. What is iteration?
 
-  Your answer:
+  Your answer: A sequence of repeated tasks to carry out. Those tasks being a series of steps/instructions.
 
-  Researched answer:
-
-
+  Researched answer: Upon futher research, I have read many defintions and they are all relatively the same. An iteration is the process of repeating a procedure a certain number of times, or until a condition is met.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: The roles of pair programming include a "driver" and a "navigator". The driver is responsible for having their hands on the keyboard and doing the actual computer work and the navigator is responsible for watching the driver. More specifically, the navigator is watching for spelling/puncutaion errors as well as guiding the driver in the appropriate direction. Both roles are equally important and if done correctly can result in very efficient work.
 
-  Researched answer:
-
-
+  Researched answer: Pair programming is a good strategy for dividing the workload evenly. The approach requires a lot of communication. The roles should be switched every 10 to 20 minutes, with multiple breaks. The driver is responsible for entering the information and following the instructions from the navigator.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: These can take functions as parameters and return a function.
 
-2. Jest:
+2. Jest: Jest is a JavaScript testing framework. This is useful to check if your code is correct.
 
-3. Objects:
+3. Objects: Objects are a JavaScript data type that can store various keyed collections with complexity.
 
-4. Method:
+4. Method: A method is a function as a property of an object.
 
-5. Classes:
+5. Classes: A template/blueprint for creating objects. They are good for excapsulating data and functions that manipulate other data.
